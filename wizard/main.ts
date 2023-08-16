cbbdf0fbfeaa7f99b99f2270563beb8e3f68e480
fknipp <franz@qnipp.com>
2023-08-16 16:34:51 +0200

    Remove obsolete imports from main.ts

--- wizard/main.ts
+++ wizard/main.ts
@@ -1,7 +1,5 @@
-import messagebox from "./messagebox.ts";
 import tasks from "./tasks/index.ts";
 
-import "./style.css";
 import progress from "./progress.ts";
 
 const nextTask = tasks.find((task) => !task.condition(document));
