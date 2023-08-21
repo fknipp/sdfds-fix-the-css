32e0fcaf1beecb8c5b0d2580a809a52f315b200c
fknipp <franz@qnipp.com>
2023-08-21 21:49:51 +0200

    Fix the description of 1em

--- wizard/tasks/t41_about_width.ts
+++ wizard/tasks/t41_about_width.ts
@@ -12,7 +12,7 @@ export default {
       <p>Take a look at this delicate CSS witchcraft in the rules with the selectors starting with <b>#about</b>.</p>
       <p>Resize the window to see a little bit of responsive web design.</p>
       <p>This block about me is little bit to broad. Could you resize the <b>width</b> to <b>20em</b>?</p>
-      <p>By the way, 1em is the height of a letter.</p>
+      <p>By the way, 1em is just the current font size.</p>
       `,
     });
   }
