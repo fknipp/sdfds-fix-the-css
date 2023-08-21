c87e8359dc1955f0938849e483fc7d4f102773cf
fknipp <franz@qnipp.com>
2023-08-21 21:42:37 +0200

    Add hint to reload the preview to update the message

--- wizard/tasks/t20_fontsize.ts
+++ wizard/tasks/t20_fontsize.ts
@@ -13,6 +13,7 @@ export default {
       <p>Could you change the <b>font size</b> for the complete page to <b>18px</b> to make it better readable for my old aunt?</p>
       <p>This would be a great improvement.</p>
       <p>Hint: Find the CSS rules for <b>body</b> and insert the declaration there.</p>
+      <p>You must reload the preview to update my message after CSS changes.</p>
       `,
     });
   },
